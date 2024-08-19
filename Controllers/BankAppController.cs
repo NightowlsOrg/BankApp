dabcbb44d266108b02f3ba35a5139e51d01b89ca
kjspong <kjspong@protonmail.com>
2024-08-19 20:43:53 +0200

    BAN-27 Create ASP.Net Core MVC web app

---
+++ Controllers/BankAppController.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using BankApp.Models;
+
+namespace BankApp.Controllers;
+
+public class BankAppController : Controller
+{
+    private readonly ILogger<BankAppController> _logger;
+
+    public BankAppController(ILogger<BankAppController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
