9e1930978cf7cfbd5c6757cb2dba15165235021a
Hussein <kotrani@live.se>
2024-09-08 11:33:26 +0200

    Update KundService.cs
    updated class

--- Application/KundService.cs
+++ Application/KundService.cs
@@ -27,4 +27,13 @@ public class KundService : IKundService
         var validatedKund = await _kundRepository.ValidateKundAsync(kund);
         return validatedKund == null ? null : new KundDTO { Id = validatedKund.Id, Lösenord = validatedKund.Lösenord, Personnummer = validatedKund.Personnummer, Förnamn = validatedKund.Förnamn, Efternamn = validatedKund.Efternamn, Adress = validatedKund.Adress, Postnummer = validatedKund.Postnummer, Postort = validatedKund.Postort, Tele = validatedKund.Tele, Epost = validatedKund.Epost };
     }
+
+    // AddKundAsync används för att lägga till en ny kund i databasen
+
+    public async Task<KundDTO> AddKundAsync(KundDTO kund)
+    {
+        var newKund = new Kund(Guid.NewGuid(), kund.Lösenord, kund.Personnummer, kund.Förnamn, kund.Efternamn, kund.Adress, kund.Postnummer, kund.Postort, kund.Tele, kund.Epost);
+        await _kundRepository.AddAsync(newKund);
+        return new KundDTO { Id = newKund.Id, Lösenord = newKund.Lösenord, Personnummer = newKund.Personnummer, Förnamn = newKund.Förnamn, Efternamn = newKund.Efternamn, Adress = newKund.Adress, Postnummer = newKund.Postnummer, Postort = newKund.Postort, Tele = newKund.Tele, Epost = newKund.Epost };
+    }
 }
