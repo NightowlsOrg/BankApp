e7cdde96681c326ac1ac63469a0123e85e405b23
Hussein <kotrani@live.se>
2024-09-08 11:34:05 +0200

    Update KundRepository.cs
    updated the class

--- Infrastructure/KundRepository.cs
+++ Infrastructure/KundRepository.cs
@@ -1,6 +1,10 @@
-using BankApp.Domain;
-using Microsoft.EntityFrameworkCore;
-
+   using BankApp.Domain;
+    using BankApp.Models;
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
 namespace BankApp.Infrastructure;
 
 // Implementerar Interface-klassen IKundRepository
@@ -34,4 +38,31 @@ public class KundRepository : IKundRepository
         var dataModel = await _context.Kunder.FirstOrDefaultAsync(k => k.Förnamn == kund.Förnamn && k.Lösenord == kund.Lösenord);
         return dataModel == null ? null : new Kund(dataModel.Id, dataModel.Lösenord, dataModel.Personnummer, dataModel.Förnamn, dataModel.Efternamn, dataModel.Adress, dataModel.Postnummer, dataModel.Postort, dataModel.Tele, dataModel.Epost);
     }
+
+    public async Task<Kund?> AddAsync(Kund kund)
+    {
+        var dataModel = new KundDataModel
+        {
+            Id = kund.Id,
+            Lösenord = kund.Lösenord,
+            Personnummer = kund.Personnummer,            Förnamn = kund.Förnamn,
+            Efternamn = kund.Efternamn,
+            Adress = kund.Adress,
+            Postnummer = kund.Postnummer,
+            Postort = kund.Postort,
+            Tele = kund.Tele,
+            Epost = kund.Epost
+        };
+
+        _context.Kunder.Add(dataModel);
+        await _context.SaveChangesAsync();
+
+        return new Kund(dataModel.Id, dataModel.Lösenord, dataModel.Personnummer, dataModel.Förnamn, dataModel.Efternamn, dataModel.Adress, dataModel.Postnummer, dataModel.Postort, dataModel.Tele, dataModel.Epost);
+    }
 }
+
+
+
+
+
+
