e8612d49e5d2e161c2ac4396d541e3124a78909d
Hussein <kotrani@live.se>
2024-09-08 11:35:43 +0200

    Update KundEntityJson.cs
    updated the class

--- Infrastructure/KundEntityJson.cs
+++ Infrastructure/KundEntityJson.cs
@@ -1,37 +1,103 @@
-using System.Text.Json.Serialization;
+// using BankApp.Domain;
+// using System.Text.Json;
 
-namespace BankApp.Infrastructure;
+// namespace BankApp.Infrastructure;
 
-// Mappar innehållet i JSON-filen kunder.json till klassen KundEntityJson
-public class KundEntityJson
+// public class KundRepositoryJson : IKundRepository
+// {
+//     private readonly string _filePath = "kunder.json";
+
+//     public Kund GetKundById(int id)
+//     {
+//         var kunder = ReadFromFile();
+//         var kundData = kunder.Find(k => k.Id == id);
+
+//         return kundData != null ? new Kund(kundData.Personnummer, kundData.Förnamn, kundData.Efternamn, kundData.Adress, kundData.Postnummer, kundData.Postort, kundData.Tele, kundData.Epost) : null;
+//     }
+
+//     private List<KundEntityJson> ReadFromFile()
+//     {
+//         if (File.Exists(_filePath))
+//         {
+//             var json = File.ReadAllText(_filePath);
+//             return JsonSerializer.Deserialize<List<KundEntityJson>>(json);
+//         }
+
+//         return new List<KundEntityJson>();
+//     }
+// }
+
+
+
+
+
+
+
+
+
+/*using BankApp.Domain;
+using Newtonsoft.Json; // Make sure to install Newtonsoft.Json package
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankApp.Infrastructure
 {
-    [JsonPropertyName("id")]
-    public int Id { get; set; }
+    public class KundRepositoryJson : IKundRepository
+    {
+        private readonly string _filePath;
 
-    [JsonPropertyName("lösenord")]
-    public string? Lösenord { get; set; }
+        public KundRepositoryJson(string filePath)
+        {
+            _filePath = filePath;
+        }
 
-    [JsonPropertyName("personnummer")]
-    public string? Personnummer { get; set; }
+        private List<Kund> LoadData()
+        {
+            if (!File.Exists(_filePath))
+            {
+                return new List<Kund>();
+            }
 
-    [JsonPropertyName("förnamn")]
-    public string? Förnamn { get; set; }
+            var json = File.ReadAllText(_filePath);
+            return JsonConvert.DeserializeObject<List<Kund>>(json) ?? new List<Kund>();
+        }
 
-    [JsonPropertyName("efternamn")]
-    public string? Efternamn { get; set; }
+        private void SaveData(List<Kund> kunder)
+        {
+            var json = JsonConvert.SerializeObject(kunder, Formatting.Indented);
+            File.WriteAllText(_filePath, json);
+        }
 
-    [JsonPropertyName("adress")]
-    public string? Adress { get; set; }
+        public Task<Kund?> GetByIdAsync(Guid id)
+        {
+            var kunder = LoadData();
+            var kund = kunder.FirstOrDefault(k => k.Id == id);
+            return Task.FromResult(kund);
+        }
 
-    [JsonPropertyName("postnummer")]
-    public string? Postnummer { get; set; }
+        public Task<IEnumerable<Kund>> GetAllAsync()
+        {
+            var kunder = LoadData();
+            return Task.FromResult(kunder.AsEnumerable());
+        }
 
-    [JsonPropertyName("postort")]
-    public string? Postort { get; set; }
+        public Task<Kund?> ValidateKundAsync(string förnamn, string lösenord)
+        {
+            var kunder = LoadData();
+            var kund = kunder.FirstOrDefault(k => k.Förnamn == förnamn && k.Lösenord == lösenord);
+            return Task.FromResult(kund);
+        }
 
-    [JsonPropertyName("tele")]
-    public string? Tele { get; set; }
-
-    [JsonPropertyName("epost")]
-    public string? Epost { get; set; }
+        public Task AddAsync(Kund kund)
+        {
+            var kunder = LoadData();
+            kunder.Add(kund);
+            SaveData(kunder);
+            return Task.CompletedTask;
+        }
+    }
 }
+*/
